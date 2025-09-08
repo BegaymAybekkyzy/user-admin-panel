6aaa66d03097bfdf59a695ec70adf0f35ba83ef7
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-09-08 18:12:00 +0600

    [frontend] Add ESLint and Prettier setup with base rules

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import css from "@eslint/css";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { ignores: ["node_modules"] },
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+    plugins: { js, prettier: pluginPrettier },
+    extends: [js.configs.recommended, prettier],
+    rules: {
+      "prettier/prettier": "error",
+      semi: ["error", "always"],
+      "react-hooks/exhaustive-deps": "error",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "warn",
+      "prefer-const": "error",
+      "eqeqeq": ["error", "always"],
+      "no-trailing-spaces": "error",
+      "keyword-spacing": ["error", { before: true, after: true }],
+    },
+  },
+
+  {
+    files: ["**/*.css"],
+    language: "css/css",
+    plugins: { css },
+    extends: ["css/recommended"],
+  },
+]);
